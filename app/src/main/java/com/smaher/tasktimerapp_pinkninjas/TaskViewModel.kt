edb51d66048365939d48bfccee11a465cc87129c
Safa <sfalaqi@gmail.com>
2021-11-03 14:13:11 +0300

    add View model

---
+++ app/src/main/java/com/smaher/tasktimerapp_pinkninjas/TaskViewModel.kt
@@ -0,0 +1,39 @@
+package com.smaher.tasktimerapp_pinkninjas
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.smaher.tasktimerapp_pinkninjas.database.Task
+import com.smaher.tasktimerapp_pinkninjas.database.TaskDatabase
+import com.smaher.tasktimerapp_pinkninjas.database.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModel (application: Application) : AndroidViewModel(application){
+    val tasks: LiveData<List<Task>>
+    val repository:TaskRepository
+
+    init{
+        val dao = TaskDatabase.getInstance(application).taskDao()
+        repository = TaskRepository(dao)
+        tasks = repository.getTasks
+    }
+
+
+    fun deleteNote(task: Task) = viewModelScope.launch( Dispatchers.IO ){
+        repository.delete(task)
+    }
+
+    fun updateNote(task: Task) = viewModelScope.launch( Dispatchers.IO ){
+        repository.update(task)
+    }
+
+    fun addNote(task: Task) = viewModelScope.launch( Dispatchers.IO ){
+        repository.insert(task)
+    }
+
+    fun deleteAll() = viewModelScope.launch( Dispatchers.IO ){
+        repository.deleteAll()
+    }
+}
